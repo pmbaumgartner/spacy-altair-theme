dd56115af10fd246684ff82a2297c7cfbbad0d51
Peter Baumgartner <5107405+pmbaumgartner@users.noreply.github.com>
2022-01-26 15:09:32 -0500

    basic test

--- tests/test_spacy_altair_theme.py
+++ tests/test_spacy_altair_theme.py
@@ -1,5 +1,17 @@
+import altair as alt
+import spacy_altair_theme
 from spacy_altair_theme import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_base_theme():
+    r = alt.themes.enable("spacy-base")
+    assert r.registry.active == "spacy-base"
+
+
+def test_monotheme():
+    r = alt.themes.enable("spacy-mono")
+    assert r.registry.active == "spacy-mono"
